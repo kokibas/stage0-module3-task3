f8fdd4b32b0fac82b83dbe8b626be15d24787bb3
kokibas <nurym0807@gmail.com>
2022-10-19 14:31:43 +0600

    Module 3 task3

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -1,7 +1,10 @@
 package lang.print.gaps.task3;
 
 public class BasicVariablesInitialization {
-    public static void main(String[] args) {
-        //write code here
+    public static void main(String[] args){
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        System.out.println(first + "\n" + second + "\n" + third);
     }
 }
